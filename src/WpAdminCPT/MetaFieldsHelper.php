683edee9499e5dd238882a2a573c633855960b77
alessandro stoppato <alexrah@gmail.com>
2022-10-20 10:49:26 +0200

    src/WpAdminCPT/MetaFieldsHelper.php:convertDateTimeZone()

--- src/WpAdminCPT/MetaFieldsHelper.php
+++ src/WpAdminCPT/MetaFieldsHelper.php
@@ -1,6 +1,11 @@
 <?php
 namespace WpAdminCPT;
 
+use DateTimeZone;
+use DateTime;
+use WP_Post;
+use Exception;
+
 class MetaFieldsHelper{
 
 	/**
@@ -31,7 +36,11 @@ class MetaFieldsHelper{
 	}
 
 
-
+	/**
+	 * @param $sValidation string
+	 *
+	 * @return string
+	 */
 	public static function getValidation($sValidation){
 		if(empty($sValidation)){
 			return '';
@@ -53,12 +62,59 @@ class MetaFieldsHelper{
 		return wp_list_pluck($aLocations,'ID');
 	}
 
+
 	/**
 	 * @return array
 	 */
 	public static function getOrganizationIds(){
 		$aOrganizations  =  get_posts( 'post_type=organization&posts_per_page=-1' );
 		return wp_list_pluck($aOrganizations,'ID');
+	}
+
+
+	/**
+	 * @param string|int $mDateTime can be a string in 2022-02-01 format or an int timestamp
+	 * @param string $sOutFormat either date as 2022-02-01 or timestamp or text as localized string 13 Febbraio 2022
+	 * @return string see $sOutFormat
+	 */
+	public static function convertDateTimeZone($mDateTime, $sOutFormat = 'date'){
+
+		try {
+
+			$oDateTime = new DateTime();
+			$oDateTime->setTimezone(new DateTimeZone('Europe/Rome'));
+
+			if(is_numeric($mDateTime)){
+
+				$oDateTime->setTimestamp($mDateTime);
+
+			} elseif(!empty($mDateTime)) {
+
+				$aDateTime = explode('-',$mDateTime);
+				$oDateTime->setDate($aDateTime[0],$aDateTime[1],$aDateTime[2]);
+				$oDateTime->setTime(0,0);
+
+			} else {
+
+				return '';
+
+			}
+
+			switch ($sOutFormat){
+				case 'timestamp':
+					return $oDateTime->format('U');
+				case 'text':
+					return mysql2date("d F Y", $oDateTime->format('Y-m-d'));
+				case 'date':
+				default:
+					return $oDateTime->format('Y-m-d');
+			}
+
+		}  catch (Exception $e){
+//	        print_r($e->getMessage());
+			return '';
+		}
+
 	}
 
 }
