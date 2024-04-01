ddae7d519aba1c1cda2d69311877ca3d3389d5e0
alessandro stoppato <alexrah@gmail.com>
2024-04-01 23:21:09 +0200

    src/types.ts: ProcessEnv: define types for .env file

--- frontend/dataGrid/src/types.ts
+++ frontend/dataGrid/src/types.ts
@@ -8,4 +8,16 @@ declare global {
     env?: tEnv
   }
   const APP_MODE: 'production'|'development';
+
+  namespace NodeJS {
+    interface ProcessEnv {
+      DEV_TEST_URL: string,
+      DEV_TEST_SERVER_IP: string,
+      DEV_TEST_SERVER_PORT: string,
+      DEV_TEST_SERVER_USERNAME: string,
+      DEV_TEST_SERVER_SSH_KEY_PATH: string,
+      DEV_TEST_SERVER_UPLOAD_PATH: string
+    }
+  }
+
 }
