76960c520415efdf98fa0a56867813403251b1f3
alessandro stoppato <alexrah@gmail.com>
2024-04-03 02:58:45 +0200

    frontend/dataGrid/src/App.tsx: add logic to add more <CouncilorForm/>

--- frontend/dataGrid/src/App.tsx
+++ frontend/dataGrid/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import TestForm from "./TestForm";
+import styled from "@emotion/styled";
+import CouncilorForm from "./CouncilorForm";
 import {tCouncilor} from "./types";
 import logger from "@alexrah/logger";
 
@@ -7,16 +8,35 @@ export default function App(){
 
   const lg = new logger();
 
+  const [countNew,setCountNew] = React.useState(0);
+
   const metaValues:tCouncilor[] = Object.values(window.wpAdminCPT['comunali_listaeletto-candidati_comunali']);
   lg.i('metaValues',metaValues);
 
+  const handleAddCouncilor = (e) => {
+    e.preventDefault();
+    setCountNew(countNew => countNew+1);
+  }
+
+  lg.i('metaValues',metaValues);
+  lg.i('countNew',countNew);
+
+  const AppContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  `
+
   return (
-    <>
+    <AppContainer>
       {metaValues.map((metaValue,index) => {
-        return <TestForm key={index} id={index} data={metaValue}/>
+        return <CouncilorForm key={`stored-${index}`} id={index} data={metaValue}/>
       })}
-      <TestForm id={metaValues.length} data={null}/>
-    </>
+      {[...Array(countNew).keys()].map(key => {
+        return <CouncilorForm key={`new-${key}`} id={metaValues.length+key} data={null}/>
+      } )}
+      <button onClick={handleAddCouncilor}>Aggiungi Consigliere</button>
+    </AppContainer>
   )
 
 }
