3085ba14b5fb5b8e0487273f57b14286d44ea31c
alessandro stoppato <alexrah@gmail.com>
2026-04-18 14:06:31 +0200

    fix(MetaFields): WpAdminCPT/MetaFieldsManager.php $aArgsArr: fix array shape

--- backend/WpAdminCPT/MetaFieldsManager.php
+++ backend/WpAdminCPT/MetaFieldsManager.php
@@ -11,7 +11,7 @@ class MetaFieldsManager{
 
 	/**
 	 * @param string $sPrefix a global prefix to prepend to all meta Name
-	 * @param array{Name:string,Label:string,LabelPublic:string,Placeholder:string,Type:string,Validation:string,Group:string} $aArgsArr[]
+	 * @param array{array{Name:string,Label:string,LabelPublic:string,Placeholder:string,Type:string,Validation:string,Group:string}} $aArgsArr
 	 */
 	public function __construct($sPrefix,$aArgsArr) {
 
